68d74a06920b0e9e4d6b02419bffb200740d8809
Alex Viktorov <viktorovaleksandr7@gmail.com>
2021-01-18 16:40:40 +0200

    fix syntax js

--- docs/js/task.js
+++ docs/js/task.js
@@ -1,9 +1,9 @@
-const addButton = document.querySelector('.btn');
-const input = document.querySelector('.form-control');
-const ul = document.querySelector('ul');
-const emptyLi = document.querySelector('.js-li');
+const addButton = document.querySelector('#js-add-todo');
+const input = document.querySelector('.js-input-todo');
+const ul = document.querySelector('.js-ul-list');
+const plugList = document.querySelector('.js-plug-list');
 
-function onAddBtnTask() {
+function onAddTodo() {
 	const li = document.createElement('li');
 	li.className = `list-group-item list-group-item-action d-flex justify-content-between rounded-pill`;
 	li.textContent = input.value;
@@ -14,18 +14,18 @@ function onAddBtnTask() {
 	ul.appendChild(li).append(closeButton);
 	input.value = '';
 
-	deleteTask(closeButton);
-	eventAddTask(li);
+	onClickCloseButton(closeButton);
+	onAddFirstTask(li);
 }
 
-function deleteTask(button) {
+function onClickCloseButton(button) {
    button.addEventListener('click',() => button.parentElement.remove());
 }
 
-function eventAddTask(list) {
+function onAddFirstTask(list) {
    if(list.classList.contains('list-group-item')) {
-   	emptyLi.remove();
+   	plugList.remove();
    }
 }
 
-addButton.addEventListener('click', onAddBtnTask);
+addButton.addEventListener('click', onAddTodo);
