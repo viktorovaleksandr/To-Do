48c0a4cca8d9d31eb594adc469410ac060981076
Alex Viktorov <viktorovaleksandr7@gmail.com>
2021-01-19 19:32:45 +0200

    upgrade todo

--- docs/js/task.js
+++ docs/js/task.js
@@ -1,31 +1,39 @@
-const addButton = document.querySelector('#js-add-todo');
+const buttonAdd = document.querySelector('#js-add-todo');
 const input = document.querySelector('.js-input-todo');
 const ul = document.querySelector('.js-ul-list');
 const plugList = document.querySelector('.js-plug-list');
 
-function onAddTodo() {
+function onCreateList() {
 	const li = document.createElement('li');
-	li.className = `list-group-item list-group-item-action d-flex justify-content-between rounded-pill`;
+	li.className = `list-group-item list-group-item-secondary 
+	d-flex justify-content-between rounded-pill`;
 	li.textContent = input.value;
-	
+	ul.append(li);
+	onCreateButtonClose(li);
+	onReplacePlugList(li);
+}
+
+function onCreateButtonClose(list) {
 	const closeButton = document.createElement('button');
 	closeButton.className = 'btn-close';
-	
-	ul.appendChild(li).append(closeButton);
-	input.value = '';
-
-	onClickCloseButton(closeButton);
-	onAddFirstTask(li);
+	list.append(closeButton);
 }
 
-function onClickCloseButton(button) {
-   button.addEventListener('click',() => button.parentElement.remove());
-}
-
-function onAddFirstTask(list) {
+function onReplacePlugList(list) {
    if(list.classList.contains('list-group-item')) {
    	plugList.remove();
    }
 }
 
-addButton.addEventListener('click', onAddTodo);
+ul.addEventListener('click', (event) => {
+	const li = event.target.closest('li');
+	const closeButton = event.target.closest('button');
+	
+	li.classList.toggle('list-group-item-info');
+
+	if (closeButton) {
+		closeButton.parentElement.remove();
+	}
+});
+
+buttonAdd.addEventListener('click', onCreateList);
