eeda6ac3b0e5108c19824e7bc0e4a207653ef642
Alex Viktorov <viktorovaleksandr7@gmail.com>
2021-01-20 20:54:14 +0200

    add icons trash

--- docs/js/task.js
+++ docs/js/task.js
@@ -1,40 +1,42 @@
 const buttonAdd = document.querySelector('#js-add-todo');
 const input = document.querySelector('.js-input-todo');
 const ul = document.querySelector('.js-list-todo');
-const plugList = document.querySelector('.js-plug-todo');
+const emptyMessage = document.querySelector('.js-empty-message');
 
-function onCreateList() {
+function onAddNewTodo() {
 	const li = document.createElement('li');
 	li.className = `list-group-item list-group-item-secondary 
 	d-flex justify-content-between rounded-pill`;
 	li.textContent = input.value;
-	ul.append(li);
-	
-	onCreateButtonClose(li);
-	onReplacePlugList(li);
+	ul.prepend(li);
+	input.value = '';
+
+	// removeEmptyMessage(li);
+	createButtonClose(li);
+	emptyMessage.remove();
 }
 
-function onCreateButtonClose(list) {
-	const closeButton = document.createElement('button');
-	closeButton.className = 'btn-close';
+function createButtonClose(list) {
+	const closeButton = document.createElement('i');
+	closeButton.className = 'bi bi-trash';
 	list.append(closeButton);
 }
 
-function onReplacePlugList(list) {
-   if(list.classList.contains('list-group-item')) {
-   	plugList.remove();
-   }
-}
+// function removeEmptyMessage(list) {
+// 	if(list.closest('.list-group-item')) {
+// 	}
+// }
 
 ul.addEventListener('click', (event) => {
 	const li = event.target.closest('li');
-	const closeButton = event.target.closest('button');
+	const closeButton = event.target.closest('i');
 	
 	li.classList.toggle('list-group-item-info');
+	li.classList.toggle('text-decoration-line-through');
 
 	if (closeButton) {
 		closeButton.parentElement.remove();
 	}
 });
 
-buttonAdd.addEventListener('click', onCreateList);
+buttonAdd.addEventListener('click', onAddNewTodo);
