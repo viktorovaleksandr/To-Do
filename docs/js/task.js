de11bb2ca21ecc7fcbaedf6303807bbab9350d42
Alex Viktorov <viktorovaleksandr7@gmail.com>
2021-01-21 22:13:19 +0200

    add show/hide code

--- docs/js/task.js
+++ docs/js/task.js
@@ -5,38 +5,41 @@ const emptyMessage = document.querySelector('.js-empty-message');
 
 function onAddNewTodo() {
 	const li = document.createElement('li');
-	li.className = `list-group-item list-group-item-secondary 
-	d-flex justify-content-between rounded-pill`;
+	li.className = `list-group-item list-group-item-secondary   
+	list-group-item-action d-flex justify-content-between rounded-pill`;
 	li.textContent = input.value;
 	ul.prepend(li);
 	input.value = '';
 
-	// removeEmptyMessage(li);
 	createButtonClose(li);
 	emptyMessage.remove();
 }
 
 function createButtonClose(list) {
 	const closeButton = document.createElement('i');
-	closeButton.className = 'bi bi-trash';
+	closeButton.className = 'bi bi-trash-fill';
 	list.append(closeButton);
 }
 
-// function removeEmptyMessage(list) {
-// 	if(list.closest('.list-group-item')) {
-// 	}
-// }
+function showEmptyMessage() {
+	if (ul.children.length < 1) {
+		ul.append(emptyMessage);
+	}  
+}
 
 ul.addEventListener('click', (event) => {
 	const li = event.target.closest('li');
 	const closeButton = event.target.closest('i');
-	
+
 	li.classList.toggle('list-group-item-info');
 	li.classList.toggle('text-decoration-line-through');
 
 	if (closeButton) {
 		closeButton.parentElement.remove();
 	}
+
+	showEmptyMessage();
 });
 
 buttonAdd.addEventListener('click', onAddNewTodo);
+
